6f9a77e0cc1792542f55dc31c1ca87175fdf42df
Chris Fang <chrisfanght@gmail.com>
2016-11-03 06:42:52 +0000

    add tests

--- test_database.py
+++ test_database.py
@@ -1,9 +1,22 @@
 import unittest
 from app.db import database, cursor
 
-class DatabaseTestCase(unittest.TestCase):
+class TestDatabase(unittest.TestCase):
 
-	def test_cursor(self):
-		sql_command = "Select * from Stops"
+	def test_database_size(self):
+		sql_command = "SELECT * FROM Stops"
 		cursor.execute(sql_command)
-		assert cursor.rowcount == 2497
+		row = cursor.fetchall()
+		self.assertEqual(len(row), 2497)
+
+	def test_retrival(self):
+		sql_command = "SELECT * FROM Stops WHERE StopId = '150DOD:5'"
+		cursor.execute(sql_command)
+		row = cursor.fetchall();
+		self.assertEqual(len(row), 1)
+		self.assertEqual(row[0][0], '150DOD:5')
+		self.assertEqual(row[0][1], 'MTD2634')
+
+
+if __name__ == '__main__':
+	unittest.main()
