f6316c9853fda95fea06ac2247fd4e885503ed8e
Chris <Chris@ChisMacbook.local>
2016-11-02 22:50:56 -0500

    post get data method

---
+++ get_data.py
@@ -0,0 +1,10 @@
+import requests
+import json
+API_KEY = "540c9808c82b43928e0c63aeed553f33"
+STOPS_DATA_URL = "https://developer.cumtd.com/api/v2.2/JSON/GetStops"
+
+parameters = {"key" : API_KEY}
+response = requests.get(STOPS_DATA_URL, params=parameters)
+data = response.json()
+with open("data.json", "w") as outfile:
+	json.dump(data, outfile, indent=4, sort_keys=True)
